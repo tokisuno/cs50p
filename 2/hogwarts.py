5a19cee6418bb1a23952af7735104f3cdc7af62c
tokisuno <lucaswmei@gmail.com>
2024-03-29 15:02:54 -0400

    adding files for lesson 2

---
+++ 2/hogwarts.py
@@ -0,0 +1,10 @@
+students = ["Hermione", "Harry", "Ron"]
+
+
+for student in students:
+    print(student)
+
+# Both do the same thing
+
+for i in range(len(students)):
+    print(i + 1, students[i])
