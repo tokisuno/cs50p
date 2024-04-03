7867b424384cc400e97a38a70dada1a6e9da9f94
tokisuno <lucaswmei@gmail.com>
2024-04-03 11:06:15 -0400

    changed fruits to global variabel in nutrition

--- 2/pset/nutrition/nutrition.py
+++ 2/pset/nutrition/nutrition.py
@@ -1,26 +1,27 @@
+fruits = [
+          {"apple": 130},
+          {"avocado": 50},
+          {"banana": 110},
+          {"cantaloupe": 50},
+          {"grapefruit": 60},
+          {"grapes": 90},
+          {"honeydew melon": 50},
+          {"kiwifruit": 90},
+          {"lemon": 15},
+          {"lime": 90},
+          {"nectarine": 15},
+          {"orange": 20},
+          {"peach": 60},
+          {"pear": 100},
+          {"pineapple": 50},
+          {"plums": 70},
+          {"strawberries": 50},
+          {"sweet cherries": 100},
+          {"tangerine": 50},
+          {"watermelon": 80},
+]
+
 def main():
-    fruits = [
-              {"apple": 130},
-              {"avocado": 50},
-              {"banana": 110},
-              {"cantaloupe": 50},
-              {"grapefruit": 60},
-              {"grapes": 90},
-              {"honeydew melon": 50},
-              {"kiwifruit": 90},
-              {"lemon": 15},
-              {"lime": 90},
-              {"nectarine": 15},
-              {"orange": 20},
-              {"peach": 60},
-              {"pear": 100},
-              {"pineapple": 50},
-              {"plums": 70},
-              {"strawberries": 50},
-              {"sweet cherries": 100},
-              {"tangerine": 50},
-              {"watermelon": 80},
-             ]
     user_input = str(input("Item: ")).lower()
     get_cals(user_input, fruits)
 
