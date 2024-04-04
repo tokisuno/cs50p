f17ed411b4518bd12b72b74847a0b3aa5a52fadc
tokisuno <lucaswmei@gmail.com>
2024-04-04 11:34:12 -0400

    started taqueria

---
+++ 3/pset/taqueria/taqueria.py
@@ -0,0 +1,25 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+
+def main():
+    return
+
+# User inputs item
+# Program in loop returns total of the items added together
+# Program remembers the added amount
+# Case insensitive
+# Runs until the user hits ends program
+
+
+if __name__ == "__main__":
+    main()
