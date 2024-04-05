abcbfa7393eb6721a3c25c014812d913ea2dc610
tokisuno <lucaswmei@gmail.com>
2024-04-05 14:32:53 -0400

    oops forgot to do math interpreter

--- 1/pset/interpreter/interpreter.py
+++ 1/pset/interpreter/interpreter.py
@@ -1,7 +1,21 @@
+
 def main():
-    x = input()
-    return 
-    #kms
+    print(float(eval(input("Expression: "))), end='')
+
+
+# def math():
+#     while True:
+#         try:
+#             x = input("Expression: ")
+#             expr = re.findall(r'[0-9\.]+|[^0-9\.]+', x)
+#             if expr[0] == "0":
+#                 return 0/int(expr[2])
+#             if int(expr[0]):
+#                 if int(expr[2]):
+#                     if expr[1] == "/" or expr[1] == "+" or expr[1] == "-" or expr[1] == "*":
+#                         return eval(x)
+#         except ValueError:
+#             return
 
 if __name__ == "__main__":
     main()
