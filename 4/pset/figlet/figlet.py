574a507c3fe1c20987044a22c23ad0a382a22c23
tokisuno <lucaswmei@gmail.com>
2024-04-09 13:49:43 -0400

    did figlet

--- 4/pset/figlet/figlet.py
+++ 4/pset/figlet/figlet.py
@@ -1,33 +1,41 @@
+
 import sys
-from pyfiglet import Figlet
 import random as rand
+import pyfiglet
+from pyfiglet import Figlet
+
+fonts = pyfiglet.FigletFont.getFonts()
 
 def main():
-    if len(sys.argv) == 1:
-        print(rand.randint(1, 2))
-
-    for arg in sys.argv[1:]:
-        if sys.argv[1] == "-f" or sys.argv[1] == "--font":
-            try:
-                res = input("Input: ")
-                fig = Figlet(font=sys.argv[2])
-                print(fig.renderText(res))
-            except TypeError:
-                return
+    for arg in sys.argv:
+        if len(sys.argv) - 1 == 2:
+            if sys.argv[2] in fonts:
+                if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+                    try:
+                        res = input("Input: ")
+                        fig = Figlet(font=sys.argv[2])
+                        print(sys.argv[2])
+                        print(fig.renderText(res))
+                    except (TypeError, IndexError):
+                        sys.exit(0)
+                    else:
+                        sys.exit(0)
+                else:
+                    sys.exit(1)
             else:
-                return
+                sys.exit(1)
+        elif len(sys.argv) - 1 == 1:
+            sys.exit(1)
         else:
             try:
                 res = input("Input: ")
-                fig = Figlet()
+                fig = Figlet(font=fonts[rand.randint(0, len(fonts))-1])
                 print(fig.renderText(res))
             except TypeError:
-                return
+                sys.exit(0)
             else:
-                return
-
+                sys.exit(1)
     return
-
 
 if __name__ == "__main__":
     main()
