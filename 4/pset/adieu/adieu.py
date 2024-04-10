b3f920a5b208b08c2c3c056a830329c43c6b8579
tokisuno <lucaswmei@gmail.com>
2024-04-10 12:17:39 -0400

    i hate my life

--- 4/pset/adieu/adieu.py
+++ 4/pset/adieu/adieu.py
@@ -1,27 +1,23 @@
-def main():
-    user_input = names()
-    if len(user_input) <= 2:
-        print("Adieu, adieu, to " + str(user_input[0] + "and " + str(user_input[1])))
-    elif len(user_input) >= 3:
-        print(user_input)
-        listed = ', '.join(user_input[:-1])
-        print("Adieu, adieu, to " + listed + ", and " + user_input[-1])
-    else:
-        return 
+import sys
 
-def names():
+
+def main():
     name_list = list()
     while True:
         try:
             x = input("Name: ").strip().capitalize()
             name_list.append(x)
-        except ValueError:
-            pass
-        except KeyError:
-            pass
         except EOFError:
-            print("")
             break
-    return name_list
+
+    if len(name_list) <= 2:
+        print("\nAdieu, adieu, to " + str(name_list[0] + " and " + str(name_list[1])))
+        sys.exit(0)
+    elif len(name_list) >= 3:
+        listed = ', '.join(name_list[:-1])
+        print("\nAdieu, adieu, to " + listed + ", and " + name_list[-1])
+        sys.exit(0)
+
+
 if __name__ == "__main__":
     main()
