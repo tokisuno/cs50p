dbe0247d7cdab41eb14ad2da134f2b28be15758a
tokisuno <lucaswmei@gmail.com>
2024-04-12 07:46:04 -0400

    a

--- 4/pset/adieu/adieu.py
+++ 4/pset/adieu/adieu.py
@@ -8,15 +8,16 @@ def main():
             x = input("Name: ").strip().capitalize()
             name_list.append(x)
         except EOFError:
-            break
-
-    if len(name_list) <= 2:
-        print("\nAdieu, adieu, to " + str(name_list[0] + " and " + str(name_list[1])))
-        sys.exit(0)
-    elif len(name_list) >= 3:
-        listed = ', '.join(name_list[:-1])
-        print("\nAdieu, adieu, to " + listed + ", and " + name_list[-1])
-        sys.exit(0)
+            if len(name_list) == 1:
+                print("\nAdieu, adieu, to " + str(name_list[0]))
+                sys.exit(0)
+            elif len(name_list) == 2:
+                print("\nAdieu, adieu, to " + str(name_list[0] + " and " + str(name_list[1])))
+                sys.exit(0)
+            elif len(name_list) >= 3:
+                listed = ', '.join(name_list[:-1])
+                print("\nAdieu, adieu, to " + listed + ", and " + name_list[-1])
+                sys.exit(0)
 
 
 if __name__ == "__main__":
---
+++ 4/pset/game/game.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+    user_input = get_input()
+
+
+def get_input():
+    while True:
+        try:
+            x = input("Level ")
+        except ValueError:
+            pass
+
+
+if __name__ == "__main__":
+    main()
